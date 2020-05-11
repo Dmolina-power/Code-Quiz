88aa032207c86a8dce2a2be232aed2f43e69e2b8
daniel molina <bigdpower@icloud.com>
2020-05-11 07:24:29 -0700

    fixed questions

--- script.js
+++ script.js
@@ -11,49 +11,59 @@
 var questions = [
   {
     question: "Where are B/W Tegus origianlly from?",
-    answer1: "Venezuelan",
-    answer2: "Argentina",
-    answer3: "Florida",
-    answer4: "Chile",
-    answer: "1",
+    answers:{
+    a: "Venezuelan",
+    b: "Argentina",
+    c: "Florida",
+    d: "Chile",
+    },
+    correctAnswer: "b"
   },
   {
     question: "what should the Temp be in the hot side of enclosure?",
-    answer1: "80 to 90 %",
-    answer2: "90 to 100%",
-    answer3: "95 to 105%",
-    answer4: "110-120",
-    answer: "3",
+    answer:{
+    a: "80 to 90 %",
+    b: "90 to 100%",
+    c: "95 to 105%",
+    d: "110-120",
+    },
+    CorrectAnswer: "d"
   },
   {
     question: "What is a Tegu's lifespan?",
-    answer1: "5 Years",
-    answer2: "10 Years",
-    answer3: "15 Years",
-    answer4: "20 Years",
-    answer: "3",
+    answer:{
+    a: "5 Years",
+    b: "10 Years",
+    c: "15 Years",
+    d: "20 Years",
+    },
+    correctAnswer: "d"
   },
   {
     question: "What are Tegu's average length?",
-    answer1: "3 to 4 feet",
-    answer2: "4 to 5 feet",
-    answer3: "5 to 6 feet",
-    answer4: "6+ feet",
-    answer: "1",
+    answer:{
+    a: "3 to 4 feet",
+    b: "4 to 5 feet",
+    c: "5 to 6 feet",
+    d: "6+ feet",
+    },
+    correctAnswer: "b"
   },
   {
     question: "Tegu's diet consits of?",
-    answer1: "raw meat and fruits",
-    answer2: "crickets and worms",
-    answer3: "vegtables",
-    answer4: "dog food",
-    answer: "0",
+    answer:{
+    a: "raw meat and fruits",
+    b: "crickets and worms",
+    c: "vegtables",
+    d: "dog food",
+    },
+    correctAnswer: "a",
   },
 ];
 // User will click on start button
 var startNow = document.querySelector("#startQuiz");
 startNow.addEventListener("click", startQuiz);
-startNow.setAttribute("style", "margin-left: 635px");
+startNow.setAttribute("style", "margin-left: 580px");
 
 var timeEl = document.querySelector(".time");
 var byeBye = document.querySelector(".jumbotron");
