0f071b820dff951556f339c5118d9c6a53baed17
daniel molina <bigdpower@icloud.com>
2020-05-10 22:02:15 -0700

    inital work

---
+++ script2.js
@@ -0,0 +1,41 @@
+var questions = [{
+    question: "Where are B/W Tegus origianlly from?",
+    answer1: "Venezuelan",
+    answer2: "Argentina",
+    answer3: "Florida",
+    answer4: "Chile",
+    answer: "1"
+},
+{
+    question: "what should the Temp be in the hot side of enclosure?",
+    answer1: "80 to 90 %",
+    answer2: "90 to 100%",
+    answer3: "95 to 105%",
+    answer4: "110-120",
+    answer: "3"
+},
+{
+    question: "What is a Tegu's lifespan?",
+    answer1: "5 Years",
+    answer2: "10 Years",
+    answer3: "15 Years",
+    answer4: "20 Years",
+    answer: "3"
+},
+{
+    question: "What are Tegu's average length?",
+    answer1: "3 to 4 feet",
+    answer2: "4 to 5 feet",
+    answer3: "5 to 6 feet",
+    answer4: "6+ feet",
+    answer: "1"
+},
+{
+    question: "Tegu's diet consits of?",
+    answer1: "raw meat and fruits",
+    answer2: "crickets and worms",
+    answer3: "vegtables",
+    answer4: "dog food",
+    answer: "0"
+}
+];
